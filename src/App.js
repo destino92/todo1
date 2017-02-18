e25944042d05e7c5d22d03814d70a0629c5f3efb
destino92 <destinodello@gmail.com>
2017-02-19 00:34:49 +0300

    feat: added a method to delete a list item

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React, { Component } from 'react';
 import './App.css';
 
-const TodoList = [
-  {
-    'id': 'list1',
-    'content': 'do something really urgent'
-  },
-  {
-    'id': 'list2',
-    'content': 'do something else'
-  }
-]
-
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      TodoList: [
+        {
+          'id': 1,
+          'content': 'do something really urgent'
+        },
+        {
+          'id': 2,
+          'content': 'do something else'
+        }
+      ]
+    }
+
+    this.removeItem = this.removeItem.bind(this);
+  }
+
+  removeItem(id){
+    var TodoList = this.state.TodoList.filter(item => item.id !== id);
+
+    this.setState({
+      TodoList: TodoList
+    });
+  }
+
   render() {
+    const {
+      TodoList
+    } = this.state;
+
     return (
       <div className="App">
         <ul>
           {TodoList.map(item =>
             <li key={item.id}>
-              {item.content}
+              {item.content} | <button onClick={ () => this.removeItem(item.id)}>x</button>
             </li>
           )}
         </ul>

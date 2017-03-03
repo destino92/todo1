f96709841d1b5e201d1d14f5c63168b60451efc7
destino92 <destinodello@gmail.com>
2017-03-03 23:51:56 +0300

    feat: removed TodoItem from app.js and put it in it's own file

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import IconDelete from 'material-ui/svg-icons/action/delete';
 import Toggle from 'material-ui/Toggle';
 import {red800, red500} from 'material-ui/styles/colors';
 import { Grid, Col, Row } from 'react-bootstrap';
+import TodoItem from 'components/TodoItem';
 
 class App extends Component {
   constructor(props) {
@@ -222,57 +223,6 @@ class App extends Component {
           </Row>
         </Grid>
       </MuiThemeProvider>
-    );
-  }
-}
-
-const TodoItem = ({ item,
-                    index,
-                    updateItem,
-                    editedContent,
-                    onEditChange,
-                    toggleEditView,
-                    toggleCompletion,
-                    removeItem
-                  }) => {
-
-  if ( item.beingEdited ) {
-    return(
-      <div>
-        <ListItem>
-            <form onSubmit={(event) => updateItem(event,item,index)}>
-              <TextField id="text-field-controlled"
-              name="inlineInput"
-              type="text"
-              value={editedContent}  onChange={onEditChange}
-              multiLine={true}
-              rows={1}
-              rowsMax={4}
-              onBlur={(event) => updateItem(event,item,index)}
-              autoFocus
-            />
-
-              <FlatButton label="Update" primary={true} type="submit"/>
-            </form>
-        </ListItem>
-        <Divider />
-      </div>
-    );
-  } else {
-    return (
-      <div>
-        <ListItem
-          rightIcon={<IconButton
-                    children={<IconDelete color={red500} hoverColor={red800} />}
-                    onClick={ () => removeItem(item.id)} />}
-        >
-          <Checkbox style={{width:"10%"}} checked={item.completed} onClick={() => toggleCompletion(item, index)} />
-          <div onDoubleClick={() =>toggleEditView(item, index)} className={item.completed ? 'completedTodo' : ''}>
-            {item.content}
-          </div>
-        </ListItem>
-        <Divider />
-      </div>
     );
   }
 }
---
+++ src/components/TodoItem.js
@@ -0,0 +1,52 @@
+const TodoItem = ({ item,
+                    index,
+                    updateItem,
+                    editedContent,
+                    onEditChange,
+                    toggleEditView,
+                    toggleCompletion,
+                    removeItem
+                  }) => {
+
+  if ( item.beingEdited ) {
+    return(
+      <div>
+        <ListItem>
+            <form onSubmit={(event) => updateItem(event,item,index)}>
+              <TextField id="text-field-controlled"
+              name="inlineInput"
+              type="text"
+              value={editedContent}  onChange={onEditChange}
+              multiLine={true}
+              rows={1}
+              rowsMax={4}
+              onBlur={(event) => updateItem(event,item,index)}
+              autoFocus
+            />
+
+              <FlatButton label="Update" primary={true} type="submit"/>
+            </form>
+        </ListItem>
+        <Divider />
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <ListItem
+          rightIcon={<IconButton
+                    children={<IconDelete color={red500} hoverColor={red800} />}
+                    onClick={ () => removeItem(item.id)} />}
+        >
+          <Checkbox style={{width:"10%"}} checked={item.completed} onClick={() => toggleCompletion(item, index)} />
+          <div onDoubleClick={() =>toggleEditView(item, index)} className={item.completed ? 'completedTodo' : ''}>
+            {item.content}
+          </div>
+        </ListItem>
+        <Divider />
+      </div>
+    );
+  }
+}
+
+export default TodoItem;
